75103804d556b7355a52a1f13839ba08899c2621
yenawee <yaena0319@naver.com>
2022-11-12 22:11:39 +0900

    cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  function getHello() {
+    const greet = document.getElementById('greet');
+    fetch('http://localhost:4000/api/hello')
+      .then((response) => response.json())
+      .then((data) => (greet.innerHTML = JSON.stringify(data)));
+  }
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(getHello);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header className="App-header">
+        <img src={reactLogo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <p>
+          api called: <code id="greet"></code>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
